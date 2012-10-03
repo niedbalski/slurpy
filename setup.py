ee160e86971a56d37af14e2600c73193363b5d36
Jorge Niedbalski R <niedbalski@gmail.com>
2012-10-03 16:21:27 -0300

    version moved to 0.1.1 after fixes

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ dependencies = [ "tornado" ]
 
 setup(
     name="slurpy",
-    version="0.1",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=dependencies,
     author="Jorge Niedbalski R.",
