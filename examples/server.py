2461df073f191292fa38c26e47a0e6320eb46367
Jorge Niedbalski R <niedbalski@gmail.com>
2012-10-03 19:14:10 -0300

    [examples] now is possible to call javascript code from python and pass a callack to process the javascript response

--- examples/server.py
+++ examples/server.py
@@ -1,9 +1,13 @@
-from slurpy import Slurpy
+import slurpy
 
 def sum(a,b):
+    slurpy.javascript.js_sum(1, 2, callback=on_js_sum_response)
     return a + b
 
-s = Slurpy()
+def on_js_sum_response(value):
+    print "The js_sum return =  %d" % value
+
+s = slurpy.Slurpy()
 s.register_method(sum)
 
 s.start()
