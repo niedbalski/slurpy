3d5a42d8c4481e08f1c986b0e6456722fb0d07c1
Jorge Niedbalski R <niedbalski@gmail.com>
2012-10-05 17:00:05 -0300

    [slurpy/examples] added dirname from os.path to examples as a registered method

--- examples/server.py
+++ examples/server.py
@@ -1,4 +1,5 @@
 import slurpy
+from os.path import dirname
 
 def sum(a,b):
     slurpy.javascript.js_sum(1, 2, callback=on_js_sum_response)
@@ -8,6 +9,8 @@ def on_js_sum_response(value):
     print "The js_sum return =  %d" % value
 
 s = slurpy.Slurpy()
+
 s.register_method(sum)
+s.register_method(dirname)
 
 s.start()
