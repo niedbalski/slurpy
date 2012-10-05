18d74dbe10b769fafba74f23fab65675eb49b9f5
Jorge Niedbalski R <niedbalski@gmail.com>
2012-10-05 16:59:25 -0300

    [slurpy] serialize.py now uses a md5sum to check for function code alteration

--- slurpy/serialize.py
+++ slurpy/serialize.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from hashlib import md5
+
 import marshal
 import json
 import types
 
 
 class CallbackSerialize:
+
+    @classmethod
+    def md5sum(cls, serialized):
+        hasher = md5()
+        hasher.update(serialized)
+        return hasher.hexdigest()
 
     @classmethod
     def serialize(cls, function):
@@ -16,12 +24,13 @@ class CallbackSerialize:
                             function.func_name,
                             function.func_defaults)
                         )
-            return serialized
+            return (cls.md5sum(serialized), serialized)
 
     @classmethod
     def deserialize(cls, encoded):
         (code, name, defaults) = json.loads(encoded)
         code = marshal.loads(code)
 
-        return types.FunctionType(code, globals=globals(), name=str(name), \
-                                                            argdefs=defaults)
+        return (cls.md5sum(encoded), \
+                types.FunctionType(code, globals=globals(), name=str(name), \
+                                                            argdefs=defaults))
